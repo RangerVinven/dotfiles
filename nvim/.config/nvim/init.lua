e58982594bd7c1f6d354da8bd6854639f5a31cec
RangerVinven <daniel.mcpherson@live.co.uk>
2025-03-26 18:02:52 +0000

    Added nvim

---
+++ nvim/.config/nvim/init.lua
@@ -0,0 +1,8 @@
+-- Load individual configurations
+require("configs.options")
+require("configs.plugins")
+require("configs.keymaps")
+require("configs.autocmds")
+require("configs.lsp")
+require("configs.colourscheme")
+
